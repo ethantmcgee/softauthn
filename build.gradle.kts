4f5b5fb8215007a5992772a3e4495cb9da57f49f
Ethan McGee <ethan.mcgee@yubico.com>
2025-03-20 10:20:21 -0400

    feat: prepare to push to maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     `java-library`
     `maven-publish`
     signing
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("com.vanniktech.maven.publish") version "0.31.0-rc2"
 }
 
-group = "io.github.adessose"
-version = "0.1.2"
+group = "dev.ethantmcgee"
+version = "0.1.3"
 
 repositories {
     mavenCentral()
@@ -17,60 +19,46 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     implementation("com.augustcellars.cose:cose-java:1.1.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc2")
-    api("com.yubico:webauthn-server-core:2.1.0")
+    api("com.yubico:webauthn-server-core:2.7.0-alpha1")
 }
 
 java {
-    withSourcesJar()
-    withJavadocJar()
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-tasks.javadoc {
-    (options as StandardJavadocDocletOptions).tags("apiNote:a:API Note:", "implNote:a:Implementation Note:")
-}
+    signAllPublications()
 
-publishing {
+    coordinates("dev.ethantmcgee", "softauthn", "0.1.3")
 
-    publications {
-        create<MavenPublication>("main") {
-            pom {
-                name.set("softauthn")
-                description.set("softauthn provides an implementation of the WebAuthn API and a software authenticator " +
-                        "in Java, using the java-webauthn-server library for data models. It can be used to test " +
-                        "WebAuthn backends.")
-                url.set("https://github.com/adessoSE/softauthn")
-                scm {
-                    url.set("https://github.com/adessoSE/softauthn")
-                }
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://mit-license.org")
-                    }
-                }
-                developers {
-                    developer {
-                        organization.set("adesso SE")
-                        organizationUrl.set("https://adesso.de")
-                    }
-                }
+    pom {
+        name.set("softauthn")
+        description.set("softauthn provides an implementation of the WebAuthn API and a software authenticator " +
+                "in Java, using the java-webauthn-server library for data models. It can be used to test " +
+                "WebAuthn backends.")
+        inceptionYear.set("2022")
+        url.set("https://github.com/ethantmcgee/softauthn")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://mit-license.org")
+                distribution.set("https://mit-license.org")
             }
-            from(components["java"])
         }
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        developers {
+            developer {
+                id.set("ethantmcgee")
+                name.set("Ethan McGee")
+                url.set("https://github.com/ethantmcgee/")
+            }
+        }
+        scm {
+            url.set("https://github.com/ethantmcgee/softauthn")
+            connection.set("scm:git:git://github.com/ethantmcgee/softauthn.git")
+            developerConnection.set("scm:git:ssh://git@github.com/ethantmcgee/softauthn.git")
         }
     }
 }
